19c7b72b8d184d1a44e23f82897ad8cb26e3529d
JhonSmith <zamuplays@gmail.com>
2024-03-01 20:43:52 -0300

    chore: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./resources/**/*.{vue,html,jsx,ts,tsx,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
